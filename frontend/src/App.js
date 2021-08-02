f6989c40d4cce8d1966fa66cec1840afe963fb4c
LilPo <tranhongquan12031999@gmail.com>
2021-08-03 00:54:40 +0700

    Tiep tuc hoan thien giao dien

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
-function App() {
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Header from './components/Header';
+import {Container} from 'react-bootstrap';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+const App = () => {
   return (
-    <>
-      <h1>Welcome to Ezzi</h1>
-    </>
+    <Router>
+      <Header/>
+      <main className="py-3">
+        <Container>
+        <Route path='/'component={HomeScreen} exact/>
+        <Route path='/product/:id'component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer/>
+    </Router>
   );
 }
 
